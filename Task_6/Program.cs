8e595b43b1d93ad4f66dba23ccc1737f92924e5e
Evgen <zhenyakeptyuha@mail.ru>
2022-05-16 21:59:42 +0300

    Add Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число ");
+int num = int.Parse(Console.ReadLine());
+int ost = num % 2;
+if (ost == 0) 
+{
+    Console.WriteLine(num + " - четное число");
+}
+else
+{
+    Console.WriteLine(num + " - нечетное число");
+}
