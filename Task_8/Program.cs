07c2950d3cf999e0d1d466a81932a4a52acafdc0
Evgen <zhenyakeptyuha@mail.ru>
2022-05-16 22:59:38 +0300

    Add Task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число ");
+float num = float.Parse(Console.ReadLine());
+float Y = 1 ;
+float ost ;
+while ( Y <= num )
+{  
+    ost = Y % 2 ;
+    if ( ost == 0 )
+    {
+       Console.Write( Y + " ");
+    } 
+        else if ( ost > 0 )
+        {
+        Console.Write("");
+        }
+        Y++;     
+}
+ Console.WriteLine( " - В промежутке от 1 до " + num + " находятся эти четные числа ");
