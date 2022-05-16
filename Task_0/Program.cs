fffd2798e9ecfc8bb4997df09687003d90bc9049
Evgen <zhenyakeptyuha@mail.ru>
2022-05-16 20:16:45 +0300

    Add Task_0

--- Task_0/Program.cs
+++ Task_0/Program.cs
@@ -1,5 +1,6 @@
-﻿Console.WriteLine("Введите число -> ");
+﻿Console.WriteLine("Введите целое число -> ");
 int num = int.Parse(Console.ReadLine());
 int numResult = num * num;
 Console.Write("Квадрат числа равен -> ");
 Console.WriteLine(numResult);
+Console.ReadKey();
