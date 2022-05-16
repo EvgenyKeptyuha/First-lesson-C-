e48d3ecb2c11449868177654d08456724b5996f5
Evgen <zhenyakeptyuha@mail.ru>
2022-05-16 20:00:59 +0300

    Add Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите первое число a -> ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число b -> ");
+int b = int.Parse(Console.ReadLine());
+bool result = b * b == a;
+Console.WriteLine(result ? "Да" : "Нет");
